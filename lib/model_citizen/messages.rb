90d271893a2a883b1bcbfe0c033b7a7761c9db51
Sylwia Olak <pumpkincouture@gmail.com>
2015-02-17 10:30:16 -0600

    Removed messages from hash, changed keys into symbols, updated tests

--- lib/model_citizen/messages.rb
+++ lib/model_citizen/messages.rb
@@ -4,32 +4,17 @@ module ModelCitizen
     attr_reader :messages
 
     def initialize
-      @messages = {"username_prompt" => "Please enter your username to get started",
-                   "choose_action" => "Please select what you'd like to do" ,
-                   "invalid_username" => "That is not a valid username, please try again",
-                   "invalid_timesheet" => "That is not a valid timesheet, please try again",
-                   "invalid_client" => "That is not a valid client, please try again",
-                   "invalid_employee" => "That is not a valid employee, please try again",
-                   "timesheet_success" => "Your timesheet has been successfully saved",
-                   "client_success" => "Client has been saved",
-                   "employee_success" => "Employee has been saved",
-                   "invalid_choice" => "That is not a valid choice, please try again",
-                   "enter_billing_type" => "Please enter project type",
-                   "enter_employee_first" => "Please enter employee first name",
-                   "enter_employee_last" => "Please enter employee last name",
-                   "enter_employee_username" => "Please enter employee username",
-                   "enter_employee_type" => "Please enter employee type (admin or non-admin)",
-                   "enter_client_name" => "Please enter client name",
-                   "enter_client_type" => "Please enter client type (if regular client, please enter 'standard')",
-                   "enter_date" => "Please input the project date using this format : YYYY/MM/DD",
-                   "future_date_error" => "You cannot log time for the future, please select a different date",
-                   "enter_hours" => "Please input hours worked",
-                   "log_time" => "Log time",
-                   "request_report" => "Request time report",
-                   "add_employee" => "Add employee",
-                   "add_client" => "Add client",
-                   "no_time_to_display" => "No time to display for this month",
-                   "request_employee_report" => "Request employee report"}
+      @messages = {
+        :invalid_username => "That is not a valid username, please try again",
+        :invalid_timesheet => "That is not a valid timesheet, please try again",
+        :invalid_client => "That is not a valid client, please try again",
+        :invalid_employee => "That is not a valid employee, please try again",
+        :timesheet_success => "Your timesheet has been successfully saved",
+        :client_success => "Client has been successfully saved",
+        :employee_success => "Employee has been successfully saved",
+        :future_date_error => "You cannot log time for the future, please select a different date",
+        :no_time_to_display => "No time to display for this month",
+        }
     end
 
     def get_message(message_choice)
--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -2,65 +2,49 @@ require "spec_helper"
 
 describe ModelCitizen::Messages do
 
-  context "displaying messages" do
-    it "should return a prompt to enter username" do
-      type_validator = ModelCitizen::Messages.new.get_message("username_prompt")
-      expect(type_validator).to eq("Please enter your username to get started")
+  context "displaying error messages" do
+
+    it "should return an error if username is invalid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:invalid_username)
+      expect(type_validator).to eq("That is not a valid username, please try again")
     end
 
-    it "should return a choose action message" do
-      type_validator = ModelCitizen::Messages.new.get_message("choose_action")
-      expect(type_validator).to eq("Please select what you'd like to do")
+    it "should return an error if timesheet is invalid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:invalid_timesheet)
+      expect(type_validator).to eq("That is not a valid timesheet, please try again")
     end
 
-    it "should return an error message" do
-      type_validator = ModelCitizen::Messages.new.get_message("invalid_choice")
-      expect(type_validator).to eq("That is not a valid choice, please try again")
+    it "should return an error if client is invalid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:invalid_client)
+      expect(type_validator).to eq("That is not a valid client, please try again")
     end
 
-    it "should return a prompt to enter employee first name" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_employee_first")
-      expect(type_validator).to eq("Please enter employee first name")
-    end
-
-    it "should return a prompt to enter employee last name" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_employee_last")
-      expect(type_validator).to eq("Please enter employee last name")
-    end
-
-    it "should return a prompt tp enter employee username" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_employee_username")
-      expect(type_validator).to eq("Please enter employee username")
-    end
-
-    it "should return a prompt to enter employee type" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_employee_type")
-      expect(type_validator).to eq("Please enter employee type (admin or non-admin)")
-    end
-
-    it "should return a prompt to enter client name" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_client_name")
-      expect(type_validator).to eq("Please enter client name")
-    end
-
-    it "should return a prompt for client type" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_client_type")
-      expect(type_validator).to eq("Please enter client type (if regular client, please enter 'standard')")
-    end
-
-    it "should return a prompt to input the date" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_date")
-      expect(type_validator).to eq("Please input the project date using this format : YYYY/MM/DD")
+    it "should return an error if employee is invalid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:invalid_employee)
+      expect(type_validator).to eq("That is not a valid employee, please try again")
     end
 
     it "should return an error for logging time in the future" do
-      type_validator = ModelCitizen::Messages.new.get_message("future_date_error")
+      type_validator = ModelCitizen::Messages.new.get_message(:future_date_error)
       expect(type_validator).to eq("You cannot log time for the future, please select a different date")
     end
+  end
 
-    it "should return a prompt for hours" do
-      type_validator = ModelCitizen::Messages.new.get_message("enter_hours")
-      expect(type_validator).to eq("Please input hours worked")
+  context "displaying success messages" do
+
+    it "should return a success message if timesheet is valid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:timesheet_success)
+      expect(type_validator).to eq("Your timesheet has been successfully saved")
+    end
+
+    it "should return a success message if client is valid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:client_success)
+      expect(type_validator).to eq("Client has been successfully saved")
+    end
+
+    it "should return a success message if employee is valid" do
+      type_validator = ModelCitizen::Messages.new.get_message(:employee_success)
+      expect(type_validator).to eq("Employee has been successfully saved")
     end
   end
 end
